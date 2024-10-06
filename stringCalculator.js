a74fb85409b3684a6cdf1e2ac5d8afab13f6ffdb
arushaHoss <hossainarusha@gmail.com>
2024-10-06 19:50:14 +0530

    Test for single string

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,11 @@
 function add(numbers) {
     if (numbers == "") {
-        return 0
+        return 0;
     }
+    else if(numbers.length == 1){
+        return parseInt(numbers); 
+    }
+    
 }
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require('./stringCalculator');
 test('Should return 0 for empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('Should return a single number for a single input', () => {
+    expect(add("1")).toBe(1);
+})
